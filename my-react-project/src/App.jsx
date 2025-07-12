5068c799511b19066c2dcaf832c6ff4d2d7a81f8
Denis Bulatov <den.bulatov.97@bk.ru>
2025-07-13 01:34:36 +0300

    Rewritten without JSX

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,66 +1,59 @@
-// декларативный
-import React, { createElement, useState } from "react";
+import { createElement } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-// императивный
 function App() {
   const [count, setCount] = useState(0);
-  const year = new Date().getFullYear();
-
-  return (
-    // декларативный
-
+  // декларативный стиль
+  return createElement(
+    "div",
+    null,
     createElement(
-      React.Fragment,
+      "div",
       null,
       createElement(
-        "div",
-        null,
-        createElement(
-          "a",
-          { href: "https://vite.dev", target: "_blank" },
-          createElement("img", {
-            src: viteLogo,
-            className: "logo",
-            alt: "Vite logo",
-          })
-        ),
-        createElement(
-          "a",
-          { href: "https://react.dev", target: "_blank" },
-          createElement("img", {
-            src: reactLogo,
-            className: "logo react",
-            alt: "React logo",
-          })
-        )
+        "a",
+        { href: "https://vite.dev", target: "_blank" },
+        createElement("img", {
+          src: viteLogo,
+          className: "logo",
+          alt: "Vite logo",
+        })
       ),
-      createElement("h1", null, "Vite + React"),
-      createElement("div", null, year),
       createElement(
-        "div",
-        { className: "card" },
-        createElement(
-          "button",
-          { onClick: () => setCount((count) => count + 1) },
-          "count is ",
-          count
-        ),
-        createElement(
-          "p",
-          null,
-          "Edit ",
-          createElement("code", null, "src/App.jsx"),
-          " and save to test HMR"
-        )
+        "a",
+        { href: "https://react.dev", target: "_blank" },
+        createElement("img", {
+          src: reactLogo,
+          className: "logo react",
+          alt: "React logo",
+        })
+      ),
+      createElement("div", null, new Date().getFullYear())
+    ),
+    createElement("h1", null, "Vite + React"),
+    createElement(
+      "div",
+      { className: "card" },
+      createElement(
+        "button",
+        { onClick: () => setCount((count) => count + 1) },
+        `count is ${count}`
       ),
       createElement(
         "p",
-        { className: "read-the-docs" },
-        "Click on the Vite and React logos to learn more"
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.jsx"),
+        " and save to test HMR"
       )
+    ),
+    createElement(
+      "p",
+      { className: "read-the-docs" },
+      "Click on the Vite and React logos to learn more"
     )
   );
 }
